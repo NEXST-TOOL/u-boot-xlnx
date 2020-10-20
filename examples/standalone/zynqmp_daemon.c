27bf3fa6e5ba10e66fe8f3b32ceb70cf644c5900
Yisong Chang <changyisong@ict.ac.cn>
2020-10-20 08:53:05 +0800

    zynqmp_daemon: Support SHA3 operations to assist RISC-V keystone framework

--- examples/standalone/zynqmp_daemon.c
+++ examples/standalone/zynqmp_daemon.c
@@ -6,6 +6,7 @@
 
 #include <common.h>
 #include <exports.h>
+#include <sha3.h>
 
 #define RV_ARM_IPC_BASE   0x80000000
 #define RV_ARM_IPC_REQ    (RV_ARM_IPC_BASE + 0x0)
@@ -19,6 +20,11 @@ static volatile u32 *ipc_arg = (void*)RV_ARM_IPC_ARG;
 static volatile u32 *ipc_ret = (void*)RV_ARM_IPC_RET;
 
 static volatile u32 *rv_reset_reg = (void*)0x83c00000;
+
+//IPC function ID
+#define SHA3_ARM_ASSIST  0xFFFF0000
+#define SHA3_UPDATE  0
+#define SHA3_FINALIZE  1
 
 int zynqmp_daemon ()
 {
@@ -42,7 +48,28 @@ int zynqmp_daemon ()
     arg1 = ipc_arg[1];
     arg2 = ipc_arg[2];
     arg3 = ipc_arg[3];
-    invoke_smc(api, arg0, arg1, arg2, arg3, ipc_ret);
+
+    if( (api & SHA3_ARM_ASSIST) == SHA3_ARM_ASSIST ) {
+      sha3_ctx_t *sha3_shared = (sha3_ctx_t *)ipc_ret;
+      api &= 0xFF;
+
+      if (api == SHA3_UPDATE) {
+        u64 len = (u64)arg2;
+
+        u64 addr = (u64)arg1 << 32;
+        addr |= arg0;
+
+        sha3_update(sha3_shared, (void *)addr, len);
+      }
+
+      else if (api == SHA3_FINALIZE)
+        sha3_final(sha3_shared);
+
+      else
+        printf ("Invalid SHA3 IPC function\n");
+    }
+    else
+      invoke_smc(api, arg0, arg1, arg2, arg3, ipc_ret);
 
     *ipc_req = 0;
   }
