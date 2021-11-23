9e937183127edf6a0bd4932ac12f43dc13082861
Yisong Chang <changyisong@ict.ac.cn>
2021-11-23 08:59:10 +0800

    riscv: serve: Fix bsp file formats for ICT SERVE platforms
    We make following modifications to support various SERVE platforms:
    
    1. define CONFIG_SYS_TEXT_BASE and CONFIG_SYS_CONFIG_NAME in
    ict_serve_XX_defconfig of each platform.
    
    The string used to define CONFIG_SYS_CONFIG_NAME is formatted as
    `ict-serve-xx`.
    Please note that there MUST be a header file
    named `ict-serve-xx.h` in include/configs/
    
    2. set CONFIG_SYS_SDRAM_BASE and script address
    in include/configs/ict-serve-xx.h
    
    3. Edit a custom boot.scr indicating the preferred boot order,
    which would be loaded as boot.scr.img via MMC or DHCP

--- include/configs/ict-serve.h
+++ include/configs/ict-serve.h
@@ -7,46 +7,22 @@
  * 
  */
 
-#ifndef __CONFIG_H
-#define __CONFIG_H
+#ifndef __ICT_SERVE_H
+#define __ICT_SERVE_H
 
 #include <linux/sizes.h>
 
-#ifdef CONFIG_SERVE_PLAT_R
-#define CONFIG_SYS_SDRAM_BASE		0x50000000UL
-#elif CONFIG_SERVE_PLAT_H
-#define CONFIG_SYS_SDRAM_BASE		0x80000000UL
-#else
-#define CONFIG_SYS_SDRAM_BASE		0xB0000000UL
-#endif
+#define CONFIG_SYS_INIT_SP_ADDR        (CONFIG_SYS_SDRAM_BASE + SZ_4M)
 
-#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_4M)
+#define CONFIG_SYS_LOAD_ADDR           (CONFIG_SYS_SDRAM_BASE + SZ_2M)
 
-#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE + SZ_2M)
+#define CONFIG_SYS_MALLOC_LEN          SZ_8M
 
-#define CONFIG_SYS_MALLOC_LEN		SZ_8M
+#define CONFIG_SYS_BOOTM_LEN           SZ_16M
 
-#define CONFIG_SYS_BOOTM_LEN		SZ_16M
-
-#ifdef CONFIG_SERVE_PLAT_R
-#define SERVE_IMAGE_ADDR \
-	"kernel_addr=0x50600000\0" \
-        "fdt_addr=0x52500000\0" \
-        "loadbootenv_addr=0x50400000\0"
-#elif CONFIG_SERVE_PLAT_H
-#define SERVE_IMAGE_ADDR \
-	"kernel_addr_r=0x80600000\0" \
-        "fdt_addr_r=0x82500000\0" \
-        "scriptaddr=0x80400000\0"
-#else
-#define SERVE_IMAGE_ADDR \
-	"kernel_addr_r=0xB0600000\0" \
-	"fdt_addr_r=0xB2500000\0" \
-        "scriptaddr=0xB0400000\0"
-#endif
-
-#define SERVE_FDT_HIGH \
-        "fdt_high=0xFFFFFFFFFFFFFFFF\0"
+#define SERVE_FDT_INITRD_HIGH \
+        "fdt_high=0xFFFFFFFFFFFFFFFF\0" \
+        "initrd_high=0xFFFFFFFFFFFFFFFF\0"
 
 #if defined(CONFIG_CMD_DHCP)
 # define BOOT_TARGET_DEVICES_DHCP(func)	func(DHCP, dhcp, na)
@@ -61,8 +37,8 @@
 
 #ifndef CONFIG_EXTRA_ENV_SETTINGS
 #define CONFIG_EXTRA_ENV_SETTINGS \
-  SERVE_IMAGE_ADDR \
-  SERVE_FDT_HIGH
+  SERVE_SCRIPT_ADDR \
+  SERVE_FDT_INITRD_HIGH
 #endif
 
-#endif /* __CONFIG_H */
+#endif /* __ICT_SERVE_H */
